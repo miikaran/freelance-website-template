89ed00cf5e979b1b6f57f74238f0cb8ad12d0e6c
miikaran <42628@edu.raseko.fi>
2022-08-22 12:37:45 +0300

    navbar reveal on scroll

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+  
+  /* =================================================================
+                  NAVBAR SCROLL REVEAL HOMMA
+   ================================================================= */
+  const body = document.body;
+  const header = document.querySelector("header");
+  const main = document.querySelector("main");
+  const headerHeight = document.querySelector("header").offsetHeight;
 
+  main.style.top = headerHeight + "px";
+
+  let lastScroll = 0;
+
+  window.addEventListener("scroll", () => {
+      let currentScroll = window.pageYOffset;
+
+      if (currentScroll - lastScroll > 0) {
+          header.classList.add("scroll-down");
+          header.classList.remove("scroll-up");
+      } 
+      else {
+          header.classList.add("scroll-up");
+          header.classList.remove("scroll-down");
+      }
+
+      lastScroll = currentScroll;
+  })
+    
+  
+  
   /* =================================================================
                   SCROLL FADE IN EFFECTI
    ================================================================= */
